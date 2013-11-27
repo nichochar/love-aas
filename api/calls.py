4c259b5563bcbc9234a9e6e9752efad5387fd860
Nicholas Charriere <nicholascharriere@gmail.com>
2013-11-27 13:23:29 +0100

    Added 2 new calls, and PIMPed the style a little

--- api/calls.py
+++ api/calls.py
@@ -18,3 +18,33 @@ class Love(webapp2.RequestHandler):
                 }
 
         self.response.out.write(template.render('templates/main_template.html', template_values))
+
+class Like(webapp2.RequestHandler):
+    def get(self, to_user, from_user):
+        """
+        Spreads the friendship from from_user to to_user
+        """
+
+        message = "Hi {to_user}, I like you, like a lot.".format(to_user=to_user)
+        signature = from_user
+        template_values = {
+                'message':message,
+                'signature':signature,
+                }
+
+        self.response.out.write(template.render('templates/main_template.html', template_values))
+
+class Miss(webapp2.RequestHandler):
+    def get(self, to_user, from_user):
+        """
+        Spreads the missing feeling from from_user to to_user
+        """
+
+        message = "Hi {to_user}, I miss you, so much.".format(to_user=to_user)
+        signature = from_user
+        template_values = {
+                'message':message,
+                'signature':signature,
+                }
+
+        self.response.out.write(template.render('templates/main_template.html', template_values))
--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,6 @@ import webapp2
 
 routes = [
         webapp2.Route(r'/love/<to_user:[A-Za-z_]+>/<from_user:[A-Za-z_]+>', handler='api.calls.Love', name='love'),
+        webapp2.Route(r'/like/<to_user:[A-Za-z_]+>/<from_user:[A-Za-z_]+>', handler='api.calls.Like', name='like'),
+        webapp2.Route(r'/miss/<to_user:[A-Za-z_]+>/<from_user:[A-Za-z_]+>', handler='api.calls.Miss', name='miss'),
 ]
